ab9ffe3e198593f901edc428050ffed2ae5f0f5d
jfernandez <jfernandez@crcit.es>
2019-12-04 07:22:48 +0100

    Mejora la gestion de datos github
    Cuando no puedo consultar datos de github si tengo la respuesta en caché
    la devuelvo antes de esperar la ventana que me facilita github

--- js-news/js/handlers/data-handler.js
+++ js-news/js/handlers/data-handler.js
@@ -9,12 +9,15 @@ const dataHandler = (() => {
   const isGithubRequest = url => {
     return url.indexOf('https://api.github.com') == 0;
   }
-  const searchGitHubData = async (url, callback) => {
+  const searchGitHubData = async (url, cacheData, callback) => {
     const rateLimitUrl = "https://api.github.com/rate_limit";
     let response = await fetch(rateLimitUrl);
     let json = await response.json();
     if (json.resources.search.remaining > 0) {
       getData(url, callback);
+      return;
+    } else if (cacheData) {
+      callback(cacheData.data);
       return;
     }
     const MILLISECONDS_TO_NEXT_SEARCH = json.resources.search.reset*1000 - Date.now();
@@ -27,8 +30,8 @@ const dataHandler = (() => {
     return MILLISECONDS_FROM_LAST_SEARCH < MILLISECONDS_CACHE_DATA_EXPIRATION; 
   }
   const getOnLineData = (url, cacheData, callback) => 
-    cacheData && areFreshCacheData(cacheData.time) ? callback(cacheData.data) : searchData(url, callback)
-  const searchData = (url, callback) => isGithubRequest(url) ? searchGitHubData(url, callback) : getData(url, callback)
+    cacheData && areFreshCacheData(cacheData.time) ? callback(cacheData.data) : searchData(url, cacheData, callback)
+  const searchData = (url, cacheData, callback) => isGithubRequest(url) ? searchGitHubData(url, cacheData, callback) : getData(url, callback)
   const getOffLineData = (url, cacheData, callback) => cacheData ? callback(cacheData.data) : notifyNoConnection()
   const notifyNoConnection = () => console.log ('Internet connection doesn\'t work')
   return {
