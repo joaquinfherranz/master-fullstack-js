051a4f5cbe05b202f3d760de5d9e4eaa92ee6f29
Joaquin F Herranz <joaquinfherranz@gmail.com>
2020-01-15 19:00:19 +0100

    calse28.2

--- notas-clase/clase28/curiosity.js
+++ notas-clase/clase28/curiosity.js
@@ -1,35 +1,46 @@
-const token = "";
-
-function NasaRequest(sol, limitRequests, frequency) {
+var NasaRequest = (sol, limitRequets, frequency) => {
+  const token = "";
   let url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='+sol+'&api_key='+token;
   console.log('request started for: ', url);
-
-  let myPromise = new Promise((resolve,reject) => {
-    fetch(url)
-      .then(res=>res.json())
-      .then(data=>resolve(data))
-      .catch(error=>reject(error))
-  });
-  myPromise
-    .then(data=>{
-      if(data.photos.length>0){
-        return data;
-      } else {
-        console.log('Delay for next request ',frequency,'ms');
-        setTimeout(NasaRequest(sol-1,limitRequests, frequency),frequency);
-      }
-    })
-    .catch(error=>console.log('Error en la consulta:',error));
   
-}; 
-
-async function init() {
-  /*
-    - current sun: 2080
-    - limit requests: false
-    - frecuency: 1000ms
-  */
-  const currentValue = await NasaRequest(2080, false, 1000);
-  console.log("currentValue:", currentValue);
+  return new Promise((resolve, reject)=>{
+    fetch(url)
+      .then(response=>response.json())
+      .then(data=>{
+        //console.log('photos=',data.photos);
+        if (data.photos.length>0) {
+          resolve (data.photos)
+        } else {
+          console.log(`Delay for next request ${frequency}ms`);
+          setTimeout(()=>resolve(NasaRequest(sol-1,limitRequets,frequency)),frequency);
+          //resolve(NasaRequest(sol-1,limitRequets,frequency));
+        }
+      })
+      .catch(error=>reject(error));
+  });
 }
-init();
+
+// var init = async () => {
+//   const currentValue = await NasaRequest(2080, false, 1000);
+//   console.log("currentValue:", currentValue);
+//   document.getElementById('imgId').setAttribute("src", currentValue[0].img_src);
+// }
+
+// init();
+
+var init = async (sol, times=0) => {
+  let newSol
+  
+    const currentValue = await NasaRequest(sol, false, 1000);
+    console.log("currentValue:", currentValue);
+    document.getElementById('imgId').setAttribute("src", currentValue[0].img_src);
+    newSol=currentValue.sol-1;
+    times=times+1
+    if (times < 10) {
+      setTimeout(()=>init(newSol, times),2000);
+    }
+    
+  
+}
+
+init(2080);
