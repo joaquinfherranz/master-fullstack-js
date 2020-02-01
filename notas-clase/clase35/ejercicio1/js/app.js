85b1bb271406cc618e8daf3f593fc4be5d7f9ab8
FullStack <fullstack@ZiMac4-3.local>
2020-02-01 14:07:15 +0100

    clase35

---
+++ notas-clase/clase35/ejercicio1/js/app.js
@@ -0,0 +1,6 @@
+//
+const emails = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com"
+
+emails.split(', ')
+
+//https://regex101.com/r/o1ADpd/3
