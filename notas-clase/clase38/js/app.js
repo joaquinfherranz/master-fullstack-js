0d8ab0831c15fa547727f4910e9bece5a3363d99
joaquinfherranz <joaquinfherranz@gmail.com>
2020-02-03 20:25:01 +0100

    clase38-ejercicio1.solucion

--- notas-clase/clase38/js/app.js
+++ notas-clase/clase38/js/app.js
@@ -7,13 +7,22 @@ const contactoHtml = {
   movil: document.getElementById("movil")  
 }
 
-const getContactos = (nombre) => {
-  return JSON.parse(localStorage.getItem(APP_KEY)) || [];
+const getCurrentContacto = () => {
+  return {
+    nombre: contactoHtml.nombre.value,
+    movil: contactoHtml.movil.value
+  }
 }
 
-const setContacto = (currContacto) => {
-  let contactos = getContactos();
+const getContactos = () => {
+  return (JSON.parse(localStorage.getItem(APP_KEY)) || [])
+    .sort((a,b)=>a.nombre<b.nombre?-1:1);
+}
+
+const setContacto = () => {
+  const currContacto = getCurrentContacto();
   if (!currContacto.nombre) return;
+  let contactos = getContactos();
   let existe = false;
   contactos = contactos.map(contacto=>{
     if (contacto.nombre==currContacto.nombre) {
@@ -21,7 +30,7 @@ const setContacto = (currContacto) => {
       return currContacto;
     } else {
       return contacto;
-    }      
+    }   
   });
   if (!existe) {
     contactos.push(currContacto);
@@ -29,32 +38,52 @@ const setContacto = (currContacto) => {
   localStorage.setItem(APP_KEY, JSON.stringify(contactos));
 }
 
+const removeContacto = () => {
+  const currContacto = getCurrentContacto();
+  if (!currContacto.nombre) return;
+  let contactos = 
+    getContactos()
+      .filter(contacto=>contacto.nombre!=currContacto.nombre);
+  localStorage.setItem(APP_KEY, JSON.stringify(contactos));
+}
+
 const renderContactos = () => {
-  //const contactos = getContactos();
   contactosHTML.innerHTML = `
     <ul>
-      ${getContactos().map(contacto=>`<li>Nombre: ${contacto.nombre}, Móvil: ${contacto.movil}</li>`).join('')}
+      ${getContactos()
+        .map((contacto,index)=>`<li>Nombre: ${contacto.nombre}, Móvil: ${contacto.movil} <input class="recuperar-contacto" type="button" data-index="${index}"></li>`)
+        .join('')}
     </ul>
-  `
+  `;
+  Array.from(document.getElementsByClassName("recuperar-contacto")).map(item=>item.addEventListener("click", (e) => {
+    const selectedIndex = e.currentTarget.getAttribute('data-index');
+    const selectedContacto = 
+      getContactos()
+      .filter((contacto,index)=>index==selectedIndex)[0];
+    contactoHtml.nombre.value=selectedContacto.nombre;
+    contactoHtml.movil.value=selectedContacto.movil;
+  }));
 }
 
 document.getElementById("guardar").addEventListener("click", () => {
-  const contacto = {
-    nombre: contactoHtml.nombre.value,
-    movil: contactoHtml.movil.value
-  }
-  setContacto(contacto);
+  setContacto();
   renderContactos();
-  // console.log("contacto:",contacto);
-  // console.log("contactos:",getContactos());
 });
 
 document.getElementById("recuperar").addEventListener("click", () => {
   renderContactos();
 });
 
+document.getElementById("eliminar").addEventListener("click", () => {
+  removeContacto();
+  renderContactos();
+});
 
 document.getElementById("eliminarTodos").addEventListener("click", () => {
   localStorage.removeItem(APP_KEY);
   renderContactos();
-})
+});
+
+// Array.from(document.getElementsByClassName("recuperar-contacto")).map(item=>item.addEventListener("click", (e) => {
+//   console.log('e',e);
+// }));
