79815bacde5a6925b5859accd1bbd1e1efa99b1c
joaquinfherranz <joaquinfherranz@gmail.com>
2020-02-03 21:57:50 +0100

    clase38 - ejercicio1
    Anade el enunciado del ejercicio

--- notas-clase/clase35/ejercicio1.js
+++ notas-clase/clase35/ejercicio1.js
@@ -1,3 +1,5 @@
+//1 - Captura los emails del siguiente texto.
+
 // tests en https://regex101.com/r/o1ADpd/9/tests
 
 const emails = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com"
