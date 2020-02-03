d991a6b70675cd600d5d85554f6e3905771c0c53
joaquinfherranz <joaquinfherranz@gmail.com>
2020-02-03 21:44:34 +0100

    clase35-ejercicio1

--- notas-clase/clase35/ejercicio1.js
+++ notas-clase/clase35/ejercicio1.js
@@ -1,7 +1,7 @@
-// tests en https://regex101.com/r/o1ADpd/8
+// tests en https://regex101.com/r/o1ADpd/9/tests
 
 const emails = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com"
 
-const patron = /[\w]+@[\w]+(\.[\w]+)+/;
+const patron = /^[\w|-]+@[\w]+(\.[\w]+)+/;
 
 emails.split(', ').filter(texto=>patron.test(texto));
