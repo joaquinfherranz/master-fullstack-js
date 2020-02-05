c00b0c8cd14df1fb5584eeae748e3595311c2b2a
joaquinfherranz <joaquinfherranz@gmail.com>
2020-02-05 21:31:03 +0100

    clase35 - ejercicio1

--- notas-clase/clase35/ejercicio1.js
+++ notas-clase/clase35/ejercicio1.js
@@ -4,6 +4,6 @@
 
 const emails = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com"
 
-const patron = /^[\w|-]+@[\w]+(\.[\w]+)+$/;
+const pattern = /^[\w|-]+@[\w]+(\.[\w]+)+$/;
 
-emails.split(', ').filter(texto=>patron.test(texto));
+emails.split(', ').filter(text=>pattern.test(text));
