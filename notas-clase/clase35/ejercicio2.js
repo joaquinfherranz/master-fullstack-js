d779dc285a040a5b29af67a0471311dfbfd7fa21
joaquinfherranz <joaquinfherranz@gmail.com>
2020-02-05 21:31:22 +0100

    clase35 - ejercicio 2

--- notas-clase/clase35/ejercicio2.js
+++ notas-clase/clase35/ejercicio2.js
@@ -4,15 +4,23 @@
 // Válidos: 12345678-A, 11223344A,
 // No válidos: A11223344, 1234567K
 
-// tests en https://regex101.com/r/CNoZ4P/1/tests
+// tests en https://regex101.com/r/CNoZ4P/2/tests
 
 const DNIs = ["12345678-A", "11223344A", "A11223344", "1234567K"];
 
-const patronDNI = /^\d{8}-?[A-Z]$/;
+const DNIpattern = /^\d{8}-?[A-Z]$/;
 
-DNIs.filter(texto=>patronDNI.test(texto));
+DNIs.filter(text=>DNIpattern.test(text));
 
 // Formato para el NIE: X-1223344-A (Guión opcional).
 // El inicio puede ser X, Y o Z.
 // Válidos: X-1234567-A, X1234567A, Z1234567M
 // No válidos: X-1233456, 1234567
+
+// https://regex101.com/r/wI1YvA/2/tests
+
+const NIEs = ["X-1234567-A", "X1234567A", "Z1234567M", "X-1233456", "1234567"];
+
+const NIEpattern = /^[XYZ]-?\d{7}-?[A-Z]$/;
+
+NIEs.filter(text=>NIEpattern.test(text));
