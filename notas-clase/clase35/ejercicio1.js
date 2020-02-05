daadb4f02ec1f93f90420b8765e31e02754f1216
Joaquin F Herranz <joaquinfherranz@gmail.com>
2020-02-05 18:49:56 +0100

    clase35 ejercicio 1 y 2

--- notas-clase/clase35/ejercicio1.js
+++ notas-clase/clase35/ejercicio1.js
@@ -1,9 +1,9 @@
 //1 - Captura los emails del siguiente texto.
 
-// tests en https://regex101.com/r/o1ADpd/9/tests
+// tests en https://regex101.com/r/o1ADpd/10/tests
 
 const emails = "demo@demo.com, demo_demo@demo.com.ar, demo-demo12312@sub.dom.com.ar, demo@novalido, novalido>@demo.com, demo@novalido-.com, demo@-novalido.com"
 
-const patron = /^[\w|-]+@[\w]+(\.[\w]+)+/;
+const patron = /^[\w|-]+@[\w]+(\.[\w]+)+$/;
 
 emails.split(', ').filter(texto=>patron.test(texto));
--- notas-clase/clase35/ejercicio2.js
+++ notas-clase/clase35/ejercicio2.js
@@ -11,3 +11,8 @@ const DNIs = ["12345678-A", "11223344A", "A11223344", "1234567K"];
 const patronDNI = /^\d{8}-?[A-Z]$/;
 
 DNIs.filter(texto=>patronDNI.test(texto));
+
+// Formato para el NIE: X-1223344-A (Guión opcional).
+// El inicio puede ser X, Y o Z.
+// Válidos: X-1234567-A, X1234567A, Z1234567M
+// No válidos: X-1233456, 1234567
