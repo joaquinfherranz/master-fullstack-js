13eeab09915d4b97fe27ee14af1bd3eb516960a9
Joaquin F Herranz <joaquinfherranz@gmail.com>
2020-03-02 19:05:33 +0100

    clase51-grunt

---
+++ notas-clase/clase51-grunt/js/ping-console.js
@@ -0,0 +1,18 @@
+// import http from 'http';
+//const http = require('http');
+const url = "fictizia.com";
+const tiempo = 3500;
+
+setInterval(() => {
+    http.get({ host: url }, res => {
+        if (res.statusCode === 200 ) {  
+            console.log(`Sin errores en ${url}`);
+        } else {
+            console.log(`Respuesta Http ${res.statusCode} en ${url}`);
+        }    
+    }).on('error', e => {
+            console.log(`Con errores -> La respuesta de ${url} es ${e.message}`  );
+    });
+}, tiempo);
+
+console.log('hola');
